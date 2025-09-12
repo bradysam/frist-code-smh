65ad62f9a4226dcc5aa9b0608f43d1ce83c642cf
bradysam <bradysam12800@gmail.com>
2025-09-12 16:46:44 +0530

    iknowshit

--- data_types/typeee.py
+++ data_types/typeee.py
@@ -1,10 +1,36 @@
 ex = 10
-print (type(ex))
+print(type(ex))
 
-bd = 7 + bj
+a = 7
+b = 9
+bd = 3 + 5j
 print (type(bd))
 
 #list type
 fe = [10,20,30,40,50,60]
 print(type(fe))
 
+# int type
+shii = 56
+print(type(shii))
+
+#float
+fii = 89.5
+print(type(fii))
+
+#string
+wee = "python is boring"
+print(type(wee))
+
+#boolen 
+money = False
+print(type(money))
+
+#list (homo)
+blue = [20,50,30,10,40]
+blue = [505,"shepy",True]
+print(type(blue))
+
+#set
+car = {"lambo","audi","bmw","ferrari"}
+print(type(car))
