847072cf290e9d36e3c1fe9cd2bc9ecbae9bedca
bradysam <bradysam12800@gmail.com>
2025-09-12 09:21:46 +0530

    worked yay

---
+++ work_pls/test1.py
@@ -0,0 +1,20 @@
+#this is test 3
+a = 10
+b = 40
+print(a+b)
+
+print ("hello")
+
+x = 10
+x += 5
+x *= 15
+print(x)
+
+d = "its raining cats and dogs here"
+print(d)
+
+o = "sheepy "
+e = "makes "
+i = "good "
+l = "turbos "
+print(o+e+i+l)
