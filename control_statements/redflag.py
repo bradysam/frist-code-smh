3c63ecb9400eaf9942e027244464435288101707
bradysam <bradysam12800@gmail.com>
2025-09-14 19:36:33 +0530

    converion

---
+++ control_statements/redflag.py
@@ -0,0 +1,34 @@
+if 5 > 2:
+    print("correct")
+if True:
+    print ("you a btixh")
+    print("dumbo")
+print ("this mf playin too much")
+
+num = 96
+if ( num > 0):
+    print("lightningMqueen")
+
+bro = 20
+if (10<= bro <= 20):
+    print("bro is legal")
+
+dude = 25
+if ( dude >= 18):
+    print("dude is chilling")
+else:
+    print("pedo")
+
+#input () :
+name = input("enter your name:")
+print("hello:"+name)
+age = input("enter your age:")
+gender = input("enter gender:")
+age = int(age)
+if (age>=21):
+    print("you are allowed to drink")
+if (age>35):
+    print("welcome unc")
+else:
+    print("go home")
+if (gender = male
--- onecode.py
+++ onecode.py
@@ -1,1 +1,4 @@
 #sample stuff
+a = 10
+b = 30
+print (a+b)
