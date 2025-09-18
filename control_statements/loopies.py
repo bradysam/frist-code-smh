822eeac17afb455197f0bbc00505ca54b5b887e1
bradysam <bradysam12800@gmail.com>
2025-09-18 14:36:28 +0530

    loopies

---
+++ control_statements/loopies.py
@@ -0,0 +1,26 @@
+counter = int(input("choose a number:"))
+while counter <=25:
+    print(counter)
+    counter = counter+2
+
+password = 1908
+user_input = 5
+
+while user_input != password:
+    user_input = int(input("ENTER YOUR PASSWORD:"))
+print("ACCESS GRANTED")
+
+#for loop
+word = "FU*K PYTHON"
+for character in word:
+    print(character)
+
+number = [1,2,3,4,5,6,7,8,9,10]
+for number in number:
+    print(number)
+
+#range (start,stop,step)
+
+pis = "what you looking at you knob"
+for times in range(6):
+    print(pis)
