72e4b59899367e0cdf58d654a391335e9395081e
bradysam <bradysam12800@gmail.com>
2025-09-27 19:06:22 +0530

    did half of the work

---
+++ strings/studentinfoenhanced.py
@@ -0,0 +1,20 @@
+print("."*50)
+print("            STUDENT INFORMATION")
+print("."*50)
+
+stu_id_valid = False
+
+while not stu_id_valid:
+    stud_id = input("Enter your ID: ")
+    if stud_id.startswith("-") and stud_id[1:].isdigit():
+        print("enter a valid id")
+    elif stud_id.isdigit():
+        stud_id = int(stud_id)
+        if stud_id > 0:
+            stu_id_valid = True
+        else:
+            print("Srudent ID can NOT be ZERO")
+    else:
+        print("enter a valid Student ID")
+else:
+    print(stud_id)
